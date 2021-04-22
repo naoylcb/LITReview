947d25e98acfd8170d1499fd179b19478f2afeca
yoan <52324469+yoanberchel@users.noreply.github.com>
2021-04-21 20:00:55 -0400

    Création des tables dans la db

--- litreview_project/subscription/models.py
+++ litreview_project/subscription/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class UserFollows(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, 
+                             related_name='following')
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, 
+                                      related_name='followed_by')
+    
+    class Meta:
+        unique_together = ('user', 'followed_user')
